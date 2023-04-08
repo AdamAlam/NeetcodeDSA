57e5d384b45cbb130b42ae4b56151c1b466d1419
Adam Alam <adamalam7@outlook.com>
2023-04-08 00:41:34 -0500

    Completed `Stacks` section

---
+++ Arrays/Stacks/Valid Parentheses.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+def isValid(self, s: str) -> bool:
+    bracket_map = {")": "(", "]": "[", "}": "{"}
+    stack = []
+
+    for c in s:
+        if c not in bracket_map:
+            stack.append(c)
+            continue
+        if not stack or stack[-1] != bracket_map[c]:
+            return False
+        stack.pop()
+
+    return not stack
+
